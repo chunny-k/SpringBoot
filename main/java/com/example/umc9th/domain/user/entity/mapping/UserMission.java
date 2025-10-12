fde62652640ea1c419e0a07e6de28fe580ac03fb
seojam03 <131682547+seojam03@users.noreply.github.com>
2025-10-12 21:44:00 +0900

    4주차 미션_잼

---
+++ main/java/com/example/umc9th/domain/user/entity/mapping/UserMission.java
@@ -0,0 +1,27 @@
+package com.example.umc9th.domain.user.entity.mapping;
+
+import com.example.umc9th.domain.mission.entity.Mission;
+import com.example.umc9th.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name = "user_mission")
+
+public class UserMission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mission_id")
+    private Mission mission;
+}
